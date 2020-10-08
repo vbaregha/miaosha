bb8e0ae93d70534134dd324369d1709e7fa45c7e
vbareghaha <lipogec842@ermailo.com>
2020-10-08 19:52:30 +0800

    md

---
+++ src/main/java/com/imooc/miaosha/controller/UserController.java
@@ -0,0 +1,32 @@
+package com.imooc.miaosha.controller;
+
+import com.imooc.miaosha.domain.MiaoshaUser;
+import com.imooc.miaosha.redis.server.RedisService;
+import com.imooc.miaosha.result.Result;
+import com.imooc.miaosha.service.MiaoshaUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+    /**
+     * 1546.6947133974695
+     * 2484.595507851322
+     */
+    @Autowired
+    MiaoshaUserService userService;
+
+    @Autowired
+    RedisService redisService;
+
+    @RequestMapping("/info")
+    @ResponseBody
+    public Result<MiaoshaUser> info(Model model, MiaoshaUser user) {
+        return Result.success(user);
+    }
+
+}
